26d6f8feec94ad82e28c93e59f7dfe548e4578c3
HEMANTH123SR <applicationcomputer257@gmail.com>
2024-07-11 19:29:02 +0530

    changed from server side to client side

--- app/[userId]/page.tsx
+++ app/[userId]/page.tsx
@@ -1,29 +1,57 @@
-import React from "react";
-import { UserDetailedObjectType } from "@/lib/types";
-import { UserComponent } from "@/components/sections/user-section"
-const Vivid = async ({ params }: { params: { userId: string } }) => {
-    const userDataResponse = await fetch(
-        `https://www.zapfolio.in/api/get-user-data?userId=${params.userId}`
-    );
+"use client";
+import React, { useState, useEffect } from "react";
+import { UserDetailedObjectType, ComponentsToShowType } from "@/lib/types";
+import { UserComponent } from "@/components/sections/user-section";
+const Vivid = ({ params }: { params: { userId: string } }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<UserDetailedObjectType | null>(null);
+  const [componentsToShow, setComponentsToShow] =
+    useState<ComponentsToShowType>({
+      summary: true,
+      headline: true,
+      geo: true,
+      languages: true,
+      educations: true,
+      position: true,
+      skills: true,
+      courses: true,
+      certifications: true,
+      projects: true,
+      resume: true,
+    });
 
-    const userData = await userDataResponse.json();
+  useEffect(() => {
+    setIsLoading(true);
+    (async () => {
+      const userDataResponse = await fetch(`/api?id=${params.userId}`);
+      const userData = await userDataResponse.json();
 
-    if (userData.success) {
-        const {
-            linkedinUserData: user,
-        }: { linkedinUserData: UserDetailedObjectType } = userData.data;
-        return (
-            <UserComponent user={user} />
-        )
+      if (userData.success) {
+        setUser(userData.data.linkedinUserData);
+        setComponentsToShow(userData.data.themesData.slate.componentsToShow);
+      }
+      setIsLoading(false);
+    })();
+  }, [params.userId]);
+
+  if (!isLoading) {
+    if (user) {
+      return <UserComponent user={user} componentsToShow={componentsToShow} />;
     }
     return (
-        <main className="flex flex-col w-screen h-screen bg-white justify-center items-center">
-            <h1 className="text-6xl font-semibold">404</h1>
-            <span className="text-xs font-semibold font-sans">
-                something went wrong
-            </span>
-        </main>
+      <main className="flex flex-col w-screen h-screen bg-white justify-center items-center">
+        <h1 className="text-6xl font-semibold">404</h1>
+        <span className="text-xs font-semibold font-sans">
+          something went wrong
+        </span>
+      </main>
     );
-}
+  }
+  return (
+    <main className="flex flex-col w-screen h-screen bg-white justify-center items-center">
+      <span className="text-xs font-semibold font-sans">loading</span>
+    </main>
+  );
+};
 
 export default Vivid;
